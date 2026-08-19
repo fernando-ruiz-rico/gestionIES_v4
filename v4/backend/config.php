c81ab541a5bffd550dcebd0a3dda9f846b26fef7
Fernando Ruiz <fernando@iessanvicente.com>
2026-08-19 16:49:18 +0200

    ip

--- v4/backend/config.php
+++ v4/backend/config.php
@@ -5,7 +5,7 @@
  */
 
 // Configuración de la base de datos
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'gestionies');
 define('DB_USER', 'root');
 define('DB_PASS', '');
